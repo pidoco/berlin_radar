a3e71ff921175f776b5b2425b1ab1c40b8d44e76
charles <charles@pidoco.com>
2022-04-29 14:48:09 +0200

    add multisite config

--- src/config/scrivito.js
+++ src/config/scrivito.js
@@ -10,4 +10,8 @@ if (process.env.SCRIVITO_ENDPOINT) {
   config.endpoint = process.env.SCRIVITO_ENDPOINT;
 }
 
+config.baseUrlForSite = () => window.location.origin;
+
+config.siteForUrl = (url) => ({ siteId: "a897dd8dbc6df808", baseUrl: window.location.origin });
+
 Scrivito.configure(config);
